c6300bf1258088a6acfb0a27b128942753d871a4
Kasie <kchaplick92@gmail.com>
2019-05-11 10:13:45 -0400

    update to connection

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers(burger_name)
+VALUES ("Royal with cheese");
+
+INSERT INTO burgers(burger_name)
+VALUES ("Big Bleu");
+
+INSERT INTO burgers(burger_name)
+VALUES ("Happy Meal");
+
+INSERT INTO burgers(burger_name)
+VALUES ("Triple Bacon");
