c6b668f43c6cf267260effc751b844b8c62cd7f0
Kasie <kchaplick92@gmail.com>
2019-05-13 11:12:52 -0400

    sql connection and orm

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers(
-  id INTEGER AUTO_INCREMENT NOT NULL,
-  burger_name VARCHAR(100) NOT NULL,
-  devoured BOOLEAN
-  PRIMARY KEY(id);
+CREATE TABLE burgers
+(
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(150) NOT NULL,
+  devoured BOOLEAN DEFAULT false,
+  PRIMARY KEY(id)
 );
